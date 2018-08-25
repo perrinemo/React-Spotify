ca163ba255c7250c25c938e9a1153443e207098f
Perrine <morandperrine@hotmail.fr>
2018-08-25 19:40:32 +0200

    mise à jour automatique du token

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-const token = "BQDCRRz45_pxO5xLUK97oCMWRSSZ-ZpYsW07NUlmaklc6LLTUYZEhrn4HCKkskVGcCOeK9QNDIf81vnupnyGJkvwh05-HH10SeiZCrpJmlxK8V-ZblVKUfjoeDCZ5Hfd1lmPn4JlF93tiU4V2Zuhhe_jh4u-W2cUfSI";
+const rawToken = "BQDCRRz45_pxO5xLUK97oCMWRSSZ-ZpYsW07NUlmaklc6LLTUYZEhrn4HCKkskVGcCOeK9QNDIf81vnupnyGJkvwh05-HH10SeiZCrpJmlxK8V-ZblVKUfjoeDCZ5Hfd1lmPn4JlF93tiU4V2Zuhhe_jh4u-W2cUfSI";
 
 export function fetchAlbums(id) {
     const url = "https://api.spotify.com/v1/artists/" + id + "/albums?include_groups=album";
-
+    var token = sessionStorage.getItem("spotify-token");
     return fetch(url, {
         method: 'get',
         headers: new Headers({
@@ -14,7 +14,7 @@ export function fetchAlbums(id) {
 
 export function fetchArtists(name) {
     const url = "https://api.spotify.com/v1/search?type=artist&q=" + name;
-
+    var token = sessionStorage.getItem("spotify-token");
     return fetch(url, {
         method: 'get',
         headers: new Headers({
